5788cfc7ef1e375b92da62bca4284726bed2b463
Sander Dijkstra <s.dijkstra02@gmail.com>
2018-03-19 23:02:32 +0100

    verder gewerkt

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -2,13 +2,13 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter { it.orderedProducts.contains(product) }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return this.orderedProducts.
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
