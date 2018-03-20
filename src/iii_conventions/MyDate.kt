af6a88e3b05d527bfb60369582e695b6a912e532
Sander Dijkstra <s.dijkstra02@gmail.com>
2018-03-20 22:41:19 +0100

    verder gewerkt

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,14 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+
+    override fun compareTo(other: MyDate): Int {
+        return (this.year*365+this.month*30+this.dayOfMonth) - (other.year*365+other.month*30+other.dayOfMonth)
+    }
+}
+
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
