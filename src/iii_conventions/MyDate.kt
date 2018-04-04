f830699367149b7b65fc7a2f7c6430dd007bf196
Sander Dijkstra <s.dijkstra02@gmail.com>
2018-04-04 22:44:37 +0200

    Tot n31 invoke

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,19 +1,29 @@
 package iii_conventions
 
-import iv_properties.toMillis
-
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-
-    override fun compareTo(other: MyDate): Int {
-        if (this.year != other.year) this.year - other.year
-        if (this.month != other.month) this.month - other.month
-        return this.dayOfMonth - other.dayOfMonth
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
     }
 }
 
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange {
-    return DateRange(this, other)
+class DateRange( val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+//class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
+//    override fun contains(value: MyDate): Boolean = start <= value && value <= endInclusive
+}
+
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var current: MyDate = dateRange.start
+    override fun next(): MyDate {
+        val result = current
+        current = current.addTimeIntervals(TimeInterval.DAY, 1)
+        return result
+    }
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
 }
 
 enum class TimeInterval {
@@ -21,29 +31,9 @@ enum class TimeInterval {
     WEEK,
     YEAR
 }
-
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
-    operator fun contains(d: MyDate): Boolean {
-        return (start.toMillis() <= d.toMillis()) && (d.toMillis() <= endInclusive.toMillis())
-    }
-
-    operator fun iterator(): MyIterator = MyIterator(this)
-//    operator fun iterator(): Iterator<MyDate> {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
-
-    class MyIterator(private val range: DateRange) : Iterator<MyDate> {
-        var cursor: MyDate = range.start
-
-        override fun hasNext(): Boolean {
-            return cursor <= range.endInclusive
-        }
-
-        override fun next(): MyDate {
-            val current = cursor
-            this.cursor = cursor.nextDay()
-            return current
-        }
-    }
-}
-
+//
+//class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+//operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+//
+//operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+//operator fun MyDate.plus(timeIntervals: RepeatedTimeInterval) = addTimeIntervals(timeIntervals.timeInterval, timeIntervals.number)
