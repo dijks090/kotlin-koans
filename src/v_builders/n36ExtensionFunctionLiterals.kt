8f3ef3acc1608cd6ac052dbce4d315010cd9caee
Dijkstra, Sander <dijkstra.s@tkppensioen.nl>
2018-04-05 17:00:36 +0200

    Extension functions

--- src/v_builders/n36ExtensionFunctionLiterals.kt
+++ src/v_builders/n36ExtensionFunctionLiterals.kt
@@ -1,5 +1,6 @@
 package v_builders
 
+import i_introduction._9_Extension_Functions.RationalNumber
 import util.TODO
 import util.doc36
 
@@ -14,8 +15,11 @@ fun todoTask36(): Nothing = TODO(
 )
 
 fun task36(): List<Boolean> {
-    val isEven: Int.() -> Boolean = { todoTask36() }
-    val isOdd: Int.() -> Boolean = { todoTask36() }
+
+    fun Int.isEven(): Boolean { return this % 2 == 0 }
+    val isEven: Int.() -> Boolean = { this % 2 == 0 }
+
+    val isOdd: Int.() -> Boolean = {  this % 2 == 1 }
 
     return listOf(42.isOdd(), 239.isOdd(), 294823098.isEven())
 }
--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -1,7 +1,6 @@
 package v_builders
 
 import util.TODO
-import java.util.*
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -27,13 +26,18 @@ fun todoTask37(): Nothing = TODO(
         building it and returning it as a result.
     """
 )
+fun <K, V> buildMap(build: MutableMap<K, V>.() -> Unit): Map<K, V> {
+    val map = HashMap<K, V>()
+    map.build()
+    return map
+}
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
+
